af0bc1d3c5ef56ada029318a543adc9f51d2caaf
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-07-25 21:25:36 +0100

    this will not work but ok

--- src/main/java/net/ccbluex/liquidbounce/ui/sound/TipSoundManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/sound/TipSoundManager.kt
@@ -13,20 +13,14 @@ class TipSoundManager {
     init {
         val enableSoundFile = File(LiquidBounce.fileManager.soundsDir, "enable.wav")
         val disableSoundFile = File(LiquidBounce.fileManager.soundsDir, "disable.wav")
-        val JelloEnableSoundFile = File(LiquidBounce.fileManager.soundsDir, "jello/enable.wav")
-        val JelloDisableSoundFile = File(LiquidBounce.fileManager.soundsDir, "jello/disable.wav")
+        val JelloEnableSoundFile = ResourceLocation("fdpclient/sound/jello/enable.wav")
+        val JelloDisableSoundFile = ResourceLocation("fdpclient/sound/jello/disable.wav")
 
         if (!enableSoundFile.exists()) {
             FileUtils.unpackFile(enableSoundFile, "assets/minecraft/fdpclient/sound/enable.wav")
         }
         if (!disableSoundFile.exists()) {
             FileUtils.unpackFile(disableSoundFile, "assets/minecraft/fdpclient/sound/disable.wav")
-        }
-        if (!JelloEnableSoundFile.exists()) {
-            FileUtils.unpackFile(JelloEnableSoundFile, "assets/minecraft/fdpclient/sound/jello/enable.wav")
-        }
-        if (!JelloDisableSoundFile.exists()) {
-            FileUtils.unpackFile(JelloDisableSoundFile, "assets/minecraft/fdpclient/sound/jello/disable.wav")
         }
 
         enableSound = TipSoundPlayer(enableSoundFile)
